b4f56e7dfe449ff0994ab2891eff7c2b7245871f
Levi-B4 <levi.butler4@gmail.com>
2023-06-16 13:38:11 -0400

    Implimented solid principles by moving methods out of the city class and creating new classes to add generics in the new CityBuilder class

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Program.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Software2KnowledgeCheck1;
+
+class Program
+{
+    public static void Main()
+    {
+        CityBuilder citybuilder = new CityBuilder();
+        var city = new City();
+        var apartment = new Apartment();
+        var highRise = new HighRise();
+        var building = new Building();
+        
+
+        citybuilder.CreateBuilding<Apartment>(apartment, city);
+
+        citybuilder.CreateBuilding(highRise, city);
+        
+        citybuilder.CreateBuilding(building, city);
+
+        foreach(Building cityBuilding in city.Buildings)
+        {
+            Console.Out.WriteLine(cityBuilding);
+        }
+    }
+}
---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Steel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class Steel : Material
+    {
+        internal override string MaterialConstructionFirstStep()
+        {
+            return "Start building structure";
+        }
+    }
+}
